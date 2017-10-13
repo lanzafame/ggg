8cc1a41c4d7fb3b7ab578863e8cfe80c5f5db22a
Adrian Lanzafame <adrianlanzafame92@gmail.com>
2017-10-13 14:08:40 +1000

    field name changed from text -> textContent

--- src/ggg/ggg.js
+++ src/ggg/ggg.js
@@ -31,7 +31,7 @@ function ggg() {
     if ($("div.btn.btn-sm.ggg-btn").length > 0) return;
 
     $("span[itemprop='keywords'].language-color").each(function () {
-        if ($(this).text() !== 'Go') return;
+        if ($(this).textContent() !== 'Go') return;
 
         $("div.select-menu-modal-holder.dropdown-menu-content.js-menu-content")
             .after("<div class='btn btn-sm ggg-btn'><div class='ggg-gopher'></div></div>");
